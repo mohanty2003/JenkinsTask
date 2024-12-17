f0437cab24e4a0e10b4d40642637e24944bfd012
Ubuntu <ubuntu@ip-172-31-12-24.ap-south-1.compute.internal>
2024-12-17 03:26:12 +0000

    script1 modified

---
+++ Script1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir dir1
+touch dir1/file1
+touch dir1/file2
+touch dir1/file3
+
+mkdir dir2
+touch dir2/file4
+touch dir2/file5
+touch dir3/file6
+
+mkdir dir3
+touch dir3/file7
+touch dir3/file8
+touch dir3/file9
